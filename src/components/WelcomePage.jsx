6be6584d6dac93275c8c57d0cf5d3487bc048b0e
David <depch47@gmail.com>
2021-11-06 20:30:00 -0500

    some css

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -2,7 +2,7 @@ import LoginButton from './LoginButton';
 
 const WelcomePage = () => {
   return (
-    <div className='w-1/2 m-auto my-10 text-center border border-black'>
+    <div className='w-full m-auto my-10 p-10 text-center border'>
       <h3 className='my-5 text-3xl'>Welcome to the store!</h3>
       <p className='my-5'>
         Please Login using a <span className='underline'>Google</span> account{' '}
